788ee1d05315f5f21c6fcc9d1cc0b7046a3d589b
Riski Dwi <bintangmalamhari@gmail.com>
2019-09-26 12:44:29 +0700

    Add files via upload

---
+++ src/latihan7/Latihan7.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan7;
+
+/**
+ *
+ * @author dwisr
+ * NAMA      : RISKI DWI SABARIYANTO
+ * KELAS     : IF -1 
+ * NIM       : 10118026
+ * Deskripsi : Program untuk belajar variabel integer
+ * 
+ */
+public class Latihan7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte b;
+        short s;
+        int i;
+        long l;
+        
+        b = 120;
+        s = 32767;
+        i = 1_00_00_000;
+        l = 9000000000000L;
+        
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int : " + i);
+        System.out.println("long : " + l);
+        
+    }
+    
+}
